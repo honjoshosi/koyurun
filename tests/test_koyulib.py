82614eec959286a58ba33470aa439e40fa91986a
honjo <honjo@sassor.com>
2024-06-20 20:45:43 +0900

    add upper

--- tests/test_koyulib.py
+++ tests/test_koyulib.py
@@ -1,9 +1,16 @@
 import unittest
 
 class TestKoyuLib(unittest.TestCase):
-    def test_koyulib(self):
+    def test_koyureverse(self):
         from koyurun.koyulib import koyureverse
         s = 'hello'
         rs = 'olleh'
         result = koyureverse(s)
+        self.assertEqual(result, rs)
+
+    def test_koyuupper(self):
+        from koyurun.koyulib import koyuupper
+        s = 'hello'
+        rs = 'HELLO'
+        result = koyuupper(s)
         self.assertEqual(result, rs)
